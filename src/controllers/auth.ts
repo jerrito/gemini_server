59281a93d7dd199cc339b5b791651054b99062de
jerrito <jerrito0240@gmail.com>
2024-06-08 22:04:13 +0000

    rf: login impl

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,5 +1,5 @@
 import express, {Request,Response, NextFunction } from "express";
-import cloudinary from 'cloudinary'
+
 import { BadRequest } from "../exceptions/bad_request";
 import { ErrorCode } from '../exceptions/root';
 import { userValidation } from "../validation/user";
@@ -7,7 +7,6 @@ import { compareSync, hashSync } from "bcrypt";
 import * as jwt from "jsonwebtoken";
 import { tokenKey } from "../secrets";
 import { NotFoundException } from "../exceptions/not_found";
-import fs from "node:fs"
 import { prisma } from "../prisma_client";
 
 export const signup=async(req:Request,res:Response,next:NextFunction)=>{
@@ -29,22 +28,14 @@ export const signup=async(req:Request,res:Response,next:NextFunction)=>{
     )
   }
 
-  const byteArrayBuffer = fs.readFileSync('../assets/god_of war.png');
-  const uploadResult = await new Promise((resolve) => {
-      cloudinary.v2.uploader.upload_stream((error, uploadResult) => {
-          return resolve(uploadResult);
-      }).end(byteArrayBuffer);
-
-  });
-
-  console.log(uploadResult);
+  
 
      user= await prisma.user.create({
         data:{
             userName,
             email:email,
             password:hashSync(password,10),
-            profile:profile 
+             
         }
     });
     res.status(200).json(user);
--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -2,7 +2,9 @@ import express, {Request,Response, NextFunction } from "express";
 import cloudinary from 'cloudinary';
 import { any } from "zod";
 import { prisma } from "../prisma_client";
-
+import { BadRequest } from "../exceptions/bad_request";
+import { ErrorCode } from "../exceptions/root";
+import fs from "node:fs"
 
 
 
@@ -22,5 +24,29 @@ export const userProfileUpdate=async(req:Request,res:Response)=>{
     res.status(200).json(user)
 
 
+}
+
+export  const updatePicture=async(req:Request, res:Response)=>{
+  
+    const {data}=req.body;
+    const byteArrayBuffer = fs.readFileSync('../assets/god_of war.png');
+    const uploadResult = await new Promise((resolve) => {
+        cloudinary.v2.uploader.upload_stream((error, uploadResult) => {
+            return resolve(uploadResult);
+        }).end(byteArrayBuffer);
+  
+    });
+  
+    console.log(uploadResult);
+    const user= await prisma.user.update({
+        where:{
+            id:req?.user?.id
+        },
+        data:{
+            profile:data
+        }
+    });
+    
+   res.status(200).json({"profile":user.profile});
 }
 
